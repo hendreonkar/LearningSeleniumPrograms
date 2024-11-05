94bca88585142db675c0cbee46962e64ee074883
hendreonkar <hendreonkar@gmail.com>
2024-11-05 23:14:31 +0530

    updating code of task2

--- src/15_Oct_task/test_task2.py
+++ src/15_Oct_task/test_task2.py
@@ -4,5 +4,7 @@ from selenium import webdriver
 def test_open_url():
     driver=webdriver.Chrome()
     driver.get("https://katalon-demo-cura.herokuapp.com/")
+    page_source_data=driver.page_source
+    assert "CURA Healthcare Service" in page_source_data
+    driver.quit()
 
-    assert driver.title == "CURA Healthcare Service"
